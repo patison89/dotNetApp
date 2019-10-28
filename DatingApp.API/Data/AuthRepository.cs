f62dd953f3c996da248b391dc0ce009e00da579e
patrycja.czerwinska <patison89@gmail.com>
2019-10-28 10:58:14 +0000

    add login repository method

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
@@ -11,9 +12,30 @@ namespace DatingApp.API.Data
         {
             _context = context;
         }
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-           
+           var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+           if (user == null)
+                return null;
+
+            if (!VerifyPasswordHash(password, user.PasswordSalt, user.PasswordHash))
+                return null;
+
+            return user;   
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordSalt, byte[] passwordHash)
+        {
+             using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt)) 
+            {
+                
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i]) return false;
+                }
+            }
+            return true;
         }
 
         public async Task<User> Register(User user, string password)
@@ -39,9 +61,12 @@ namespace DatingApp.API.Data
             
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new System.NotImplementedException();
+            if (await _context.Users.AnyAsync(x => x.Username == username))
+                return true;
+
+            return false;
         }
     }
 }
